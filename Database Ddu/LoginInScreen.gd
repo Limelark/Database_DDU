f1ffc89b068f5f547f54c98768a4f93cdd8ec65a
Nikolaj Rask <83177244+NikolajRask@users.noreply.github.com>
2023-10-30 18:26:34 +0100

    Added request

--- Database Ddu/LoginInScreen.gd
+++ Database Ddu/LoginInScreen.gd
@@ -1,13 +1,14 @@
 extends Control
 
-var username = null
+var email = null
 var password = null
 
 
-
+func _ready():
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 
 func _on_Username_text_changed(new_text):
-	username = new_text
+	email = new_text
 	
 
 func _on_Password_text_changed(new_text):
@@ -16,5 +17,8 @@ func _on_Password_text_changed(new_text):
 
 
 func _on_LoginButton_pressed():
-	print(username)
-	print(password)
+	$HTTPRequest.request("http://localhost/apps/Godot%20Server/login.php?email="+email+"&password="+password)
+
+func _on_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	print(json.result)
